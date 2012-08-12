aa5aac3967c6562e771d2c97ce292f1dccae0970
Dominique Sandoz <dominique.sandoz@gmail.com>
2012-08-12 15:21:16 +0200

    Bugfix for specified versions

--- index2.js
+++ index2.js
@@ -3,40 +3,55 @@ var server = "http://localhost:5984";
 var fetchServer = "ec2-46-137-149-160.eu-west-1.compute.amazonaws.com";
 
 var nano = require("nano")(server),
-	express = require("express");
+  express = require("express");
 
 var app = express.createServer();
 var db = nano.db.use("registry");
 app.listen(80);
 
+var adjustTarball = function(version) {
+  version.dist.tarball = version.dist.tarball.replace("registry.npmjs.org", fetchServer);
+  version.dist.tarball = version.dist.tarball.replace("\/-\/", "/");
+};
+
 app.get('/:doc/:at', function(req, res) {
-  if (isNaN(req.params.at.substring(0, 1))
+  if (isNaN(req.params.at.substring(0, 1))) {
     db.attachment.get(req.params.doc, req.params.at).pipe(res);
-  else
+  }
+  else {
     db.get(req.params.doc, function(err, body) {
       if (!err) {
-	if ("versions" in body) {
-		var versions = body.versions;
-		if (req.params.at in versions) {
-			
+        if ("versions" in body && req.params.at in body.versions) {
+          var version = body.versions[req.params.at];
+          if ("dist" in version) {
+            adjustTarball(version);
+          }
+
+          req.send(version);
+        }
+        else {
+          req.send("notfound");
+        }
+      }
+    });
+  }
 });
 
 app.get('/:doc', function(req, res){
   db.get(req.params.doc, function(err, body) {
-  	if (!err) {
-  		if ("versions" in body) {
-  			var versions = body.versions;
-  			for (var k in versions) {
-  				if ("dist" in versions[k]) {
-  					versions[k].dist.tarball = versions[k].dist.tarball.replace("registry.npmjs.org", fetchServer);
-  					versions[k].dist.tarball = versions[k].dist.tarball.replace("\/-\/", "/");
-  				}
-  			}
-  		}
-  		res.send(body);
-  	}
-  	else {
-  		res.send("notfound");
-  	}
-  })
+    if (!err) {
+      if ("versions" in body) {
+        var versions = body.versions;
+        for (var k in versions) {
+          if ("dist" in versions[k]) {
+            adjustTarball(versions[k]);
+          }
+        }
+      }
+      res.send(body);
+    }
+    else {
+      res.send("notfound");
+    }
+  });
 });
