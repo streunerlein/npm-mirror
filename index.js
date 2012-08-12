9bb5edda83fba4217feadb864ba14bd6c4045cf6
Dominique Sandoz <dominique.sandoz@gmail.com>
2012-08-12 13:05:24 +0000

    Some fixes

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 
-var server = "http://ec2-79-125-28-91.eu-west-1.compute.amazonaws.com";
+var server = "http://localhost:5984";
+var fetchServer = "ec2-46-137-149-160.eu-west-1.compute.amazonaws.com";
 
-var nano = require("nano")(server + ":80"),
+var nano = require("nano")(server),
 	express = require("express");
 
 var app = express.createServer();
 var db = nano.db.use("registry");
-app.listen(8010);
+app.listen(80);
 
-app.get('/registry/:doc', function(req, res){
-	console.log("get",req.params.doc)
+app.get('/:doc/:at', function(req, res) {
+  db.attachment.get(req.params.doc, req.params.at).pipe(res);
+});
+
+app.get('/:doc', function(req, res){
   db.get(req.params.doc, function(err, body) {
   	if (!err) {
-  		console.log(body)
   		if ("versions" in body) {
   			var versions = body.versions;
   			for (var k in versions) {
   				if ("dist" in versions[k]) {
-  					versions[k].dist.tarball = versions[k].dist.tarball.replace("registry.npmjs.org", server);
+  					versions[k].dist.tarball = versions[k].dist.tarball.replace("registry.npmjs.org", fetchServer);
   					versions[k].dist.tarball = versions[k].dist.tarball.replace("\/-\/", "/");
   				}
   			}
@@ -25,7 +28,7 @@ app.get('/registry/:doc', function(req, res){
   		res.send(body);
   	}
   	else {
-  		res.send("notfound:",err.message);
+  		res.send("notfound");
   	}
   })
 });
